ed20867f238c92be3224c1aaf383ee512a3c8c33
editpeng <36735879+editpeng@users.noreply.github.com>
2018-02-25 01:26:35 +0800

    Update StringToDateConverter.java

--- src/main/java/com/nongyou/commons/converter/StringToDateConverter.java
+++ src/main/java/com/nongyou/commons/converter/StringToDateConverter.java
@@ -18,26 +18,36 @@ public class StringToDateConverter implements Converter<String, Date> {
 		}
 
 		String[] dateEles = source.split("[^0-9]+");
-		for (int i = 0; i < dateEles.length; i++) {
-			if (i == 0) {
-				dateStr.replace(0, 4, String.format("%04d", Integer.valueOf(dateEles[0])));
+		if(dateEles.length == 1) {
+			if(dateEles[0].length()>=14) {
+				dateStr.replace(0, 14, dateEles[0].substring(0, 14));
 			}
-			if (i == 1) {
-				dateStr.replace(4, 6, String.format("%02d", Integer.valueOf(dateEles[1])));
-			}
-			if (i == 2) {
-				dateStr.replace(6, 8, String.format("%02d", Integer.valueOf(dateEles[2])));
-			}
-			if (i == 3) {
-				dateStr.replace(8, 10, String.format("%02d", Integer.valueOf(dateEles[3])));
-			}
-			if (i == 4) {
-				dateStr.replace(10, 12, String.format("%02d", Integer.valueOf(dateEles[4])));
-			}
-			if (i == 5) {
-				dateStr.replace(12, 14, String.format("%02d", Integer.valueOf(dateEles[5])));
+			else {
+				dateStr.replace(0, dateEles[0].length(), dateEles[0]);
 			}
 		}
+		else {
+			for (int i = 0; i < dateEles.length; i++) {
+				if (i == 0) {
+					dateStr.replace(0, 4, String.format("%04d", Integer.valueOf(dateEles[0])));
+				}
+				if (i == 1) {
+					dateStr.replace(4, 6, String.format("%02d", Integer.valueOf(dateEles[1])));
+				}
+				if (i == 2) {
+					dateStr.replace(6, 8, String.format("%02d", Integer.valueOf(dateEles[2])));
+				}
+				if (i == 3) {
+					dateStr.replace(8, 10, String.format("%02d", Integer.valueOf(dateEles[3])));
+				}
+				if (i == 4) {
+					dateStr.replace(10, 12, String.format("%02d", Integer.valueOf(dateEles[4])));
+				}
+				if (i == 5) {
+					dateStr.replace(12, 14, String.format("%02d", Integer.valueOf(dateEles[5])));
+				}
+			}
+		}	
 
 		try {
 			return new SimpleDateFormat("yyyyMMddHHmmss").parse(dateStr.toString());
